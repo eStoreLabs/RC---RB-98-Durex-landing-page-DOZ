2138f5ce94a7517caa81a9b3c80e9783517c2410
Radosław WIeczorek <r.p.wieczorek@gmail.com>
2025-01-27 20:26:34 +0100

    Add products ids to carousels

--- js/script.js
+++ js/script.js
@@ -84,11 +84,11 @@
 
     // Inicjalizacja wszystkich karuzel
     async function initAllCarousels() {
-      await initCarousel('carousel-1', ['181324', '145013','6551','175311','181327','181321', '53027']);
+      await initCarousel('carousel-1', ['181324', '145013','6551','175311']);
       await initCarousel('carousel-2', ['181324', '181327','181321', '181322']);
       await initCarousel('carousel-3', ['6542', '6540','148076', '145013']);
-      await initCarousel('carousel-4', ['6551', '137090','148078', '53027']);
-      await initCarousel('carousel-5', ['175311','175310','175309', '175312']);
+      await initCarousel('carousel-4', ['6551', '137090','148078', '53027','64533','13302','148077']);
+      await initCarousel('carousel-5', ['175311','175310','175309', '175312','175313','175312','148800']);
     }
 
     // Uruchomienie wszystkich karuzel
