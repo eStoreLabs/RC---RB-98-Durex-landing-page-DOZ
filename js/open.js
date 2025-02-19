204cf6f7a467f17b400ca8e91a6d8f904192b93a
Radosław WIeczorek <r.p.wieczorek@gmail.com>
2025-02-19 10:43:12 +0100

    Make corrections

--- js/open.js
+++ js/open.js
@@ -1,16 +1,1 @@
-const imgElement = document.querySelector('.es-tip__img--top');
-const windowHeight = window.innerHeight;
-
-
-function handleScroll() {
-  const scrollPosition = window.scrollY;
-  const maxMarginBottom = -150;
-  const minMarginBottom = 150;
-  const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
-  const scrollPercentage = scrollPosition / maxScroll;
-  const margin = ((1 - scrollPercentage) * (maxMarginBottom - minMarginBottom)) + minMarginBottom;
-
-  imgElement.style.marginBottom = `${margin}px`;
-}
-
-window.addEventListener('scroll', handleScroll);
+const imgElement=document.querySelector(".es-tip__img--top"),windowHeight=window.innerHeight;function handleScroll(){let e=window.scrollY,t=document.documentElement.scrollHeight-window.innerHeight;imgElement.style.marginBottom=`${-((1-e/t)*300)+150}px`}window.addEventListener("scroll",handleScroll);
